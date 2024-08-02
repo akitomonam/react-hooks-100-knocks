e3a443b8888d0b4c41564f3232396f94185bbb46
ak-satou <ak-satou@kddi-agile.com>
2024-08-02 18:25:30 +0900

    ノック2

--- react-codes/app/basic/page.js
+++ react-codes/app/basic/page.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 export default function Home() {
   const [counterNum, setCounterNum] = useState(0);
+  const [fieldText, setFieldText] = useState("どんどん編集しちゃって！！");
 
   const handleCountUp = () => {
     setCounterNum((prevCounterNum) => prevCounterNum + 1);
@@ -22,6 +23,13 @@ export default function Home() {
       <button type="button" onClick={handleCountDown}>
         down
       </button>
+      <input
+        type="text"
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder="自由に記述してください"
+        value={fieldText}
+        onChange={(e) => setFieldText(e.target.value)}
+      />
     </div>
   );
 }
